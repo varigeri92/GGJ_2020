11fd596b680650816145b445d42f1877fcc443ea
varigeri92 <varigeri92@gmail.com>
2020-02-01 13:29:54 +0100

    initial comit

---
+++ Assets/Scripts/GameSystemManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystemManager : MonoBehaviour
+{
+    //Singleton:
+
+    static GameSystemManager instance;
+    public static GameSystemManager Instance { get => instance; set => instance = value; }
+
+
+
+    public List<Wall> walls = new List<Wall>();
+
+    public Vector3 worldMousePosition;
+
+    // Test// 
+
+    public Rigidbody WreckingBall;
+
+
+    private void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+
+        foreach( Wall wall in GameObject.FindObjectsOfType<Wall>())
+        {
+            walls.Add(wall);
+        }
+    }
+
+    void OnCanonShot()
+    {
+        foreach (Wall wall in walls )
+        {
+            wall.TurnKinematic(false);
+        }
+    }
+
+    public void OnShotOver()
+    {
+        foreach (Wall wall in walls)
+        {
+            wall.TurnKinematic(true);
+        }
+    }
+
+
+    /// TEST ///
+    /// 
+
+    private void Update()
+    {
+        worldMousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            WreckingBall.AddForce(new Vector3(1,0,0) * 1, ForceMode.Impulse);
+
+            OnCanonShot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            OnShotOver();
+        }
+    }
+
+    public void CreateElement(GameObject element)
+    {
+        Instantiate(element, worldMousePosition, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    [SerializeField] WallProperties properties;
+    Rigidbody rb;
+    
+
+    public bool placed = false;
+
+    int durability;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        durability = properties.durability;
+    }
+
+    private void Update()
+    {
+        if (!placed)
+        {
+            transform.position = GameSystemManager.Instance.worldMousePosition;
+
+            if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+            {
+                placed = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("projectile"))
+        {
+            durability -= 1;
+            if (durability <= 0)
+            {
+                durability = 0;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void TurnKinematic(bool turn)
+    {
+        rb.isKinematic = turn;
+    }
+}
