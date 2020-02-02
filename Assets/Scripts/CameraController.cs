f9a75a0f60515eeea97651f0317fa4a00939f603
Markus B <>
2020-02-02 16:06:37 +0100

    Add: camera animation, animator and controller script to trigger zoom-in/out

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	private Animator animator;
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	public void ZoomIn()
+	{
+		animator.SetBool("ZoomIn", true);
+	}
+
+	public void ZoomOut()
+	{
+		animator.SetBool("ZoomIn", false);
+	}
+}
