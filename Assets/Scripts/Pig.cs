d1478fc99b98648a55e37ff09239c1722c1d4141
varigeri92 <varigeri92@gmail.com>
2020-02-02 16:55:33 +0100

    OKAY

--- Assets/Scripts/Pig.cs
+++ Assets/Scripts/Pig.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Pig : MonoBehaviour
 {
-
+    [SerializeField]
+    ParticleSystem pS;
+    [SerializeField]
+    AudioSource aS;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.gameObject.CompareTag("projectile"))
         {
+            pS.transform.parent = null;
+            aS.transform.parent = null;
+            aS.Play();
+            pS.Play();
+
             gameObject.SetActive(false);
+
         }
         if (collision.collider.gameObject.CompareTag("Wall"))
         {
